1564193c236249ac8a1cda6796e04e3184b37c42
Spencer Rinehart <anubis@overthemonkey.com>
2015-04-15 19:06:40 -0500

    Fix newrelic disabling.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('assert-dotenv')({}, function() {
   'use strict';
 
-  var newrelic = process.env.NEW_RELIC_ENABLED ? require('newrelic') : null;
+  var newrelic = process.env.NEW_RELIC_ENABLED === 'true' ?
+    require('newrelic') :
+    null;
 
   var HelpEsb = require('help-esb');
   var esbClient = new HelpEsb.Client(process.env.ESB, {
